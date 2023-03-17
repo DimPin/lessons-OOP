12af5717b8b50490365897c96bcfea496391b698
Dima Pinaev <dimapokemon005@gmail.com>
2023-03-17 20:54:28 +0300

    homework-4

--- src/homework_4/Main.java
+++ src/homework_4/Main.java
@@ -1,5 +1,30 @@
 package src.homework_4;
 
+import src.homework_4.gb.list.GBLinkedList;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        GBLinkedList<Integer> gbLinkedList = new GBLinkedList<>();
+
+        gbLinkedList.add(1).add(2).add(3);
+        gbLinkedList.addFirst(4);
+        gbLinkedList.addLast(5);
+
+        System.out.println(gbLinkedList);
+
+        gbLinkedList.remove(2);
+        System.out.println(gbLinkedList);
+
+        gbLinkedList.removeFirst();
+        System.out.println(gbLinkedList);
+
+        gbLinkedList.removeLast();
+        System.out.println(gbLinkedList);
+
+        gbLinkedList.update(0, 10);
+        System.out.println(gbLinkedList);
+
+        System.out.println(gbLinkedList.size());
+        System.out.println(gbLinkedList.get(0));
+    }
 }
--- src/homework_4/gb/GBList.java
+++ src/homework_4/gb/GBList.java
@@ -1,5 +1,10 @@
 package src.homework_4.gb;
 
-public class GBList {
-    
+public interface GBList<T> extends Iterable<T> {
+    GBList<T> add(T t);
+    GBList<T> add(int i, T t);
+    void remove(int i);
+    T get(int i);
+    int size();
+    void update(int i, T t);
 }
